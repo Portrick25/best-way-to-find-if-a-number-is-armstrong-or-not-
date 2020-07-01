ca6a1e7bc2d4124339920ec2cd3a400972001281
Ritik jha <31991503+Portrick25@users.noreply.github.com>
2020-07-02 01:31:36 +0530

    Create IsArmstrong.py

---
+++ IsArmstrong.py
@@ -0,0 +1,17 @@
+'''
+  what are Armstrong Numbers?
+  when sum of all digits of an number which are powered or exponentialised with the highest place order of the number, is equal to the actual number are said to be an armstrong number.
+'''
+
+
+n = input() #take the number as string
+l = len(n) # find the length of the number for exponential purpose
+sum = 0
+for x in n: # separate all digits
+    sum += int(x)**l #exponent them
+    
+if int(n) == sum:
+    print(str(n)+" is a armstrong number")
+
+else:
+    print(str(n) + " is not an armstron number")
